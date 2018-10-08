8be404ca66ddef5d60808d581491c2b34dafcc0b
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-10-08 19:59:43 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,10 @@ require 'nokogiri'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
 end
 binding.pry
 
-create_project_hash 
+create_project_hash
